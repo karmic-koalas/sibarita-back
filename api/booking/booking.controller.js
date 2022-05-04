9862822e727a8be5a654179c3beb86843c74233d
Erik Eduardo <erikeah@protonmail.com>
2022-05-04 18:19:18 +0100

    Bug-fix: get booking

--- api/booking/booking.controller.js
+++ api/booking/booking.controller.js
@@ -1,23 +1,22 @@
-import bookingModel from './booking.model.js'
+import bookingModel from "./booking.model.js";
 import express from "express";
 
 export function getAllBookings(req, res) {
-    return bookingModel.find().then(
-        (response) => {
-            return res.json(response)
-        }
-    )
+  return bookingModel.find().then((response) => {
+    return res.json(response);
+  });
 }
 
 // Get all bookings from one company!!!
 export function getAllBookingsByToken(req, res) {
-    return bookingModel.findById({bookingToken : req.params.bookingToken}).then(
-        (response) => {
-            if (response === null) {
-                return []
-            } else {
-                return res.json(response)
-            }
-        }
-    )
+  return bookingModel
+    .findOne({ bookingToken: req.params.bookingToken })
+    .then((response) => {
+      if (response === null) {
+        return [];
+      } else {
+        return res.json(response);
+      }
+    });
 }
+
