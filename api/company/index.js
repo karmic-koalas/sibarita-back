fa9c166d9c925df43a4b5175a996b69171d1f8d1
MaximoDM <rtp.iea2010@gmail.com>
2022-05-04 13:38:57 +0100

    Removed ununsed files

--- api/company/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-import { functionañadir, functionañadir, functionañadir, functionañadir } from './company.controller';
-
-router.get('/', functionañadir);
-router.get('/:id', functionañadir);
-router.post('/', functionañadir);
-router.delete('/:id', functionañadir);
-
-export default router;
