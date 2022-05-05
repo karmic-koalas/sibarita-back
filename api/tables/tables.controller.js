33fd1668c7e1d047eb7bae4309cf548c9379fc3a
Erik Eduardo <erikeah@protonmail.com>
2022-05-05 17:19:15 +0100

    I modified tables to better usage internally

--- api/tables/tables.controller.js
+++ api/tables/tables.controller.js
@@ -1,18 +1,30 @@
 import tablesModel from "./tables.model.js";
 
-export function getAllTables(req, res) {
-  return tablesModel.find().then((response) => {
-    return res.json(response);
-  });
-}
-
-// Get all Tables from one company!!!
-export function getAllTablesByOwner(req, res) {
-  return tablesModel.find({ owner: req.params.owner }).then((response) => {
+export function getAllTablesByOwner(owner) {
+  return tablesModel.find({ owner: owner }).then((response) => {
     if (response === null) {
       return [];
     } else {
-      return res.json(response);
+      return response;
     }
   });
+}
+
+function getAllTables(req, res) {
+  return tablesModel.find().then((response) => response);
+}
+
+/**
+ * EXPRESS
+ */
+
+// Get all Tables from one company!!!
+export function getAllTablesByOwnerAPI(req, res) {
+  return getAllTablesByOwner(req.params.owner).then((response) =>
+    res.json(response)
+  );
+}
+
+export function getAllTablesAPI(req, res) {
+  return getAllTables().then((response) => res.json(response));
 }
