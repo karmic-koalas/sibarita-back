29e395023ae5e98537cafc189aa4faa8bdaf26f9
Erik Eduardo <erikeah@protonmail.com>
2022-05-11 14:51:25 +0100

    bug-fix: method to ensure unique tokens has been implemented

--- api/booking/booking.controller.js
+++ api/booking/booking.controller.js
@@ -1,39 +1,58 @@
 import bookingModel from "./booking.model.js";
 import humanId from "../../lib/human-id/human-id.js";
 
+function getByToken(token) {
+  return bookingModel.findOne({ bookingToken: token }).then((response) => {
+    if (response === null) {
+      return false;
+    } else {
+      return response;
+    }
+  });
+}
+
+async function uniqueToken(index = 0) {
+  const token = humanId({
+    separator: "-",
+    capitalize: false,
+    adjectives: [],
+    nouns: [
+      "erik",
+      "pedrocamejo",
+      "pedrort",
+      "cesar",
+      "pedrocd",
+      "juancesar0",
+      "maximodm",
+      "deviluppercase",
+    ],
+  });
+  if (index > 4) return;
+  if ((await getByToken(token)) === false) return token;
+  return uniqueToken(index + 1);
+}
+
 /**
  * EXPRESS SECTION
  */
 
 // Get all bookings from one company!!!
-export async function getBookingsByToken(req, res) {
-  const response = await bookingModel.findOne({ bookingToken: req.params.bookingToken });
-  if (response === null) {
-    return [];
-  } else {
-    return res.json(response);
-  }
+export function getBookingsByToken(req, res) {
+  return getByToken(req.params.bookingToken)
+    .then((response) => {
+      if (response === false) return res.json([]);
+      return res.json(response);
+    })
+    .catch((error) => res.status(400).json(error));
 }
 
-export function postBooking(req, res) {
+export async function postBooking(req, res) {
+  const resultToken = await uniqueToken();
+
   const newBooking = new bookingModel({
     client: req.body.client,
     owner: req.body.owner,
-    bookingToken: humanId({
-      separator: "-",
-      capitalize: false,
-      adjectives: [],
-      nouns: [
-        "erik",
-        "pedrocamejo",
-        "pedrort",
-        "cesar",
-        "pedrocd",
-        "juancesar0",
-        "maximodm",
-        "deviluppercase",
-      ],
-    }),
+    bookingToken: resultToken,
     bookingDate: req.body.bookingDate,
     numPerson: req.body.numPerson,
   });
@@ -43,5 +62,3 @@ export function postBooking(req, res) {
     .then((result) => res.json(result))
     .catch((error) => res.status(400).json(error));
 }
-
-
