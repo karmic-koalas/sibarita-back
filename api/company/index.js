62a6687f08414b379589ec36bbec861dd31b352c
MaximoDM <rtp.iea2010@gmail.com>
2022-05-19 14:11:23 +0100

    commented unused calls

--- api/company/index.js
+++ api/company/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getAllCompanies, getCompany } from "./company.controller.js";
-import { isAuth, isAdmin } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
-router.get("/", getAllCompanies);
-router.get("/:owner", isAuth, getCompany);
+//router.get("/", getAllCompanies);
+router.get("/:owner", getCompany);
 
 export default router;
